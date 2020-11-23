3247118a0616301c9427bfb9d4af195957fd855d
Rachel Whitmore <rachel.whitmore@mandg.com>
2020-11-23 12:40:31 +0000

    Refactored ReturnAverage&NumberOfElements

--- StatsCalculator.Test/UnitTest1.cs
+++ StatsCalculator.Test/UnitTest1.cs
@@ -21,11 +21,11 @@ namespace StatsCalculator.Test
 
             Assert.AreEqual(minimum,result);
         }
+
         [TestCase(new int[]{5,8,3,2,10},10)]
         [TestCase(new int[]{1,2,3},3)]
         [TestCase(new int[]{4,5,6},6)]
         [TestCase(new int[]{7,8,9},9)]
-        
         public void ReturnMaximum_WhenSearchingForMaximum_GivenNumbers(int[] numbers, int maximum)
         {
             var result = _calculator.MaximumValueFrom(numbers);
@@ -33,64 +33,24 @@ namespace StatsCalculator.Test
             Assert.AreEqual(maximum,result);
         }
 
-        [Test]
-        public void ReturnNumberOfElementsIs4When1234()
+        [TestCase(new []{1,2,3,4},4)]
+        [TestCase(new []{3,4,10,-2},4)]
+        [TestCase(new []{-1,10,11,25,67},5)]
+        public void ReturnCount_WhenSearchingForNumberOfElements_GivenNumbers(int[] numbers, int count)
         {
-            
-            
-            var result = _calculator.NumberOfElements(1, 2, 3, 4);
-            
-            Assert.AreEqual(4, result);
+            var result = _calculator.NumberOfElements(numbers);
+
+            Assert.AreEqual(count,result);
         }
 
-        [Test]
-        public void ReturnNumberOfElementsIs2When3410Neg2()
+        [TestCase(new []{10,20,30},20)]
+        [TestCase(new []{6,-2,20,13},9.25)]
+        [TestCase(new []{4,5,6,1,2,3},3.5)]
+        public void ReturnAverage_WhenSearchingForAverage_GivenNumbers(int[] numbers, decimal average)
         {
-            
-            
-            var result = _calculator.NumberOfElements(3, 4, 10, -2);
-            
-            Assert.AreEqual(4,result);
-        }
-        [Test]
-        public void ReturnNumberOfElementsIs5()
-        {
-            
-            
-            var result = _calculator.NumberOfElements(-1, 10, 11, 25, 67);
-            
-            Assert.AreEqual(5, result);
-        }
+            var result = _calculator.AverageValue(numbers);
 
-        [Test]
-        public void ReturnAverageValue20WhenGiven102030()
-        {
-            
-            
-            var result = _calculator.AverageValue(10, 20, 30);
-            
-            Assert.AreEqual(20, result);
+            Assert.AreEqual(average,result);
         }
-
-        [Test]
-        public void ReturnAverageValue9Point23WhenGiven6Neg22013()
-        {
-            
-            
-            var result = _calculator.AverageValue(6, -2, 20, 13);
-            
-            Assert.AreEqual(9.25, result);
-        }
-
-        [Test]
-        public void ReturnAverageValue3Point5WhenGiven6Neg22013()
-        {
-            
-            
-            var result = _calculator.AverageValue(4, 5, 6, 1, 2, 3);
-            
-            Assert.AreEqual(3.5, result);
-        }
-
     }
 }
